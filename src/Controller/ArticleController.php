a503b87e6fdbf420ff001707cae6305d4074fc7e
root <root@localhost.localdomain>
2018-11-14 14:54:54 -0800

    added new article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -18,11 +18,40 @@
         * @Route("/", name="article_list")
         * @Method({"GET"})
         */ 
-        public function index() {
-
+        public function index() 
+        {
             $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
             return $this->render('articles/index.html.twig', array('articles' => $articles));
+        }        
+
+        /**
+        * @Route("/article/new", name="new_article")
+        * Method({"GET", "POST"})
+        */
+        public function new(Request $request)
+        {
+            $article = new Article();
+
+            $form = $this->createFormBuilder($article)
+                ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+                ->add('body', TextareaType::class, array('required' => false, 'attr' => array('class' => 'form-control')))
+                ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary mt-3')))
+                ->getForm();
+
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()) {
+                $article = $form->getData();
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($article);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('article_list');
+            }
+
+            return $this->render('articles/new.html.twig', array('form' => $form->createView()));
         }
 
         /**
