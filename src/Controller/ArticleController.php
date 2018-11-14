02e8a058c9cbe4978bba077536257375fd607060
root <root@localhost.localdomain>
2018-11-14 13:44:06 -0800

    init commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,37 @@
+<?php
+    namespace App\Controller;
+
+    use App\Entity\Article;
+
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
+    use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+    use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+    class ArticleController extends Controller {
+
+        /**
+        * @Route("/", name="article_list")
+        * @Method({"GET"})
+        */ 
+        public function index() {
+
+            $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+
+            return $this->render('articles/index.html.twig', array('articles' => $articles));
+        }
+
+        /**
+        * @Route("/article/{id}", name="article_show")        
+        */
+        public function show($id)
+        {
+            $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+            return $this->render('articles/show.html.twig', array('article' => $article));
+        }
+    }
